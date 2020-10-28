62f35edb3955bba57386e77a7d377e0ddf34ca47
acerjak <62491401+acerjak@users.noreply.github.com>
2020-10-28 16:14:31 -0700

    Conversation factory created

---
+++ database/factories/ConversationFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\Conversation;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ConversationFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Conversation::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+
+        return [
+            'user_id' => User::factory(),
+            'title' => $this->faker->sentence,
+            'body' => $this->faker->text,
+        ];
+    }
+}
